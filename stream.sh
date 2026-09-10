d256e0a477ea206d7799c892bea2f074e8a54113
subhadipsenmodak752-oss <subhadipsenmodak752@gmail.com>
2026-09-10 16:21:37 +0530

    stream.sh
    stream.sh

---
+++ stream.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+while true
+do
+    ffmpeg -re -stream_loop -1 -i "https://drive.google.com/file/d/1BsF_6Infx0jqiSTWpq6tst-pFbaqnHsi/view?usp=sharing" \
+    -c:v libx264 -preset veryfast -maxrate 2500k -bufsize 5000k -pix_fmt yuv420p -g 50 \
+    -c:a aac -b:a 128k -ar 44100 \
+    -f flv "rtmp://a.rtmp.youtube.com/live2/8hr2-mgzy-caw1-18q3-dd84"
+done
