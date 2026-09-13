348366771be0403ba642765616b12fcc790ea5bc
subhadipsenmodak752-oss <subhadipsenmodak752@gmail.com>
2026-09-13 15:30:20 +0530

    Update stream.sh

--- stream.sh
+++ stream.sh
@@ -4,5 +4,5 @@ do
     ffmpeg -re -stream_loop -1 -i "video.mp4" \
     -c:v libx264 -preset veryfast -maxrate 2500k -bufsize 5000k -pix_fmt yuv420p -g 50 \
     -c:a aac -b:a 128k -ar 44100 \
-    -f flv "rtmp://a.rtmp.youtube.com/live2/8hr2-mgzy-caw1-18q3-dd84"
+    -f flv "rtmp://a.rtmp.youtube.com/live2/$1"
 done
